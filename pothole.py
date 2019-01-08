518122dd75ab7d7bcac96ec3cf9080fc609bbdf5
sugamxp <sugamxp@gmail.com>
2019-01-08 18:14:46 +0530

    conv1

--- pothole.py
+++ pothole.py
@@ -359,7 +359,7 @@ if __name__ == '__main__':
     if args.weights.lower() == "coco":
         # Exclude the last layers because they require a matching
         # number of classes
-        model.load_weights(weights_path, by_name=True, exclude=[
+        model.load_weights(weights_path, by_name=True, exclude=["conv1",
             "mrcnn_class_logits", "mrcnn_bbox_fc",
             "mrcnn_bbox", "mrcnn_mask"])
     else:
